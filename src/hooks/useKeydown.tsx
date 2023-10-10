da6239cdfd1166defa617f24fb8c962eda86943f
Leonid Lezner <kontakt@lezner.de>
2023-10-10 21:34:49 +0200

    Removing unused hook

--- src/hooks/useKeydown.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-
-export function useKeydown(keys: Array<string>, callback: Function) {
-  useEffect(() => {
-    document.addEventListener("keydown", callback);
-
-    return () => {};
-  }, []);
-}
